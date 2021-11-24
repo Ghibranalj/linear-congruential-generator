b33a61b33feae291314ef0cd879182210b2f875f
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2021-11-25 05:55:14 +0700

    read seed from args

--- random.c
+++ random.c
@@ -3,26 +3,35 @@
 #include <stdio.h>
 #include <limits.h>
 #include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
 
-int rand(int, int, int, int);
+int myrand(int, int, int, int);
 // my implementation of the linear congruential Generator algorithm
 int main(int argc, char **argv){
+
+	char *seedString = "1234512";
+
+	if (argc > 1){
+		seedString = argv[1];
+	}
+
 	int a = 1103515245;
 	int b = 0;
 	int m = INT_MAX;
-	int seed = time(0);
-	
-	b = rand(a,b,m,seed);
+	int seed = atoi(seedString);
+
+	b = myrand(a,b,m,seed);
 	int curr = seed;
 
 	for (int i = 0; i < 10; i++){
 
-		curr = rand(a, b, m, curr);
+		curr = myrand(a, b, m, curr);
 		printf("%d\n", curr);
 	}
 }
 
-int rand(int a, int b, int m, int curr){
+int myrand(int a, int b, int m, int curr){
 	return a * curr + b % m;
 }
